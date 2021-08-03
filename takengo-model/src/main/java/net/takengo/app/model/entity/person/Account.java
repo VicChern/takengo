7e7e96f3303c340348aefd16e74517993465df26
vicchern <victor.cherniiavskiy@securenettech.com>
2021-08-03 16:44:36 +0300

    Initial commit

---
+++ takengo-model/src/main/java/net/takengo/app/model/entity/person/Account.java
@@ -0,0 +1,12 @@
+package net.takengo.app.model.entity.person;
+
+import net.takengo.app.model.entity.base.AbstractEntity;
+
+/**
+ * Entity that encapsulates user of the application
+ * @author admin
+ *
+ */
+public class Account extends AbstractEntity{
+
+}
